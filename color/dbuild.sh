d4c694eb015020d8c08d74e33d84e2a61a5a24c0
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-10-03 13:43:59 -0700

    Adding color version of debian slim

---
+++ color/dbuild.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -x
+
+NETWORK=${5:-host}
+
+TAG=${4:-stretch}
+CONTAINER=${3:-debslim-color-test}
+REPOSITORY=${2:-debslim-color}
+ACTION=${1:-BUILD}
+DAEMONIZE=-d
+
+# Delete test container built from docker file
+docker stop $CONTAINER
+docker rm $CONTAINER
+
+if [ "$ACTION" = "BUILD" ]; then
+# Delete test image built from docker file
+docker image rm $REPOSITORY:$TAG
+
+# Create test image from docker file
+docker build --rm -t $REPOSITORY:$TAG .
+
+ACTION=DEBUG
+
+fi
+
+if [ "$ACTION" = "DEBUG" ]; then
+    DAEMONIZE=""
+    DEBUG="--user root -it --entrypoint /bin/bash"
+fi
+
+NETWORK=host
+
+if [ "$ACTION" = "COMPOSE" ]; then
+
+docker run --user root -it --entrypoint /bin/bash \
+       --net=$NETWORK \
+       --name $CONTAINER \
+       $REPOSITORY:$TAG
+
+else
+
+docker run $DAEMONIZE $DEBUG \
+       --net=$NETWORK \
+       --name $CONTAINER \
+       $REPOSITORY:$TAG
+
+fi
