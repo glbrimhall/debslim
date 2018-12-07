121e642e92160899ad977495075ef71b83e24d07
gbrimhall <geoff@glbrimhall.com>
2018-12-07 15:52:19 -0700

    Adding python environment

---
+++ python/dbuild.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -x
+
+NETWORK=${5:-host}
+
+TAG=${4:-stretch}
+CONTAINER=${3:-debslim-python-test}
+REPOSITORY=${2:-debslim-python}
+ACTION=${1:-BUILD}
+DAEMONIZE=-d
+
+# Delete test container built from docker file
+docker stop $CONTAINER
+docker rm $CONTAINER
+
+if [ "$ACTION" = "BUILD" ]; then
+# Delete test image built from docker file
+docker image rm $REPOSITORY:$TAG
+
+# Create test image from docker file
+docker build --rm -t $REPOSITORY:$TAG .
+
+ACTION=DEBUG
+
+fi
+
+if [ "$ACTION" = "DEBUG" ]; then
+    DAEMONIZE=""
+    DEBUG='--user root -it --entrypoint "//bin//bash"'
+fi
+
+NETWORK=host
+
+if [ "xx$MSYSTEM" != "xx" ]; then
+  WINPTY='winpty'
+fi
+
+if [ "$ACTION" = "COMPOSE" ]; then
+
+$WINPTY docker run --user root -it --entrypoint "//bin//bash" \
+       --net=$NETWORK \
+       --name $CONTAINER \
+       -v 'c://://win' \
+       $REPOSITORY:$TAG
+
+else
+
+$WINPTY docker run $DAEMONIZE $DEBUG \
+       --net=$NETWORK \
+       --name $CONTAINER \
+       -v 'c://://win' \
+       $REPOSITORY:$TAG
+
+fi
