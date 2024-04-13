804092be16e06ed394e2854961041e9a51000c19
Geoff Brimhall <geoff@glbrimhall.com>
2024-04-13 12:42:32 -0700

    xrdp: Added research on creating xrdp docker container

---
+++ xrdp/run-xrdp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+start_xrdp_services() {
+    # Preventing xrdp startup failure
+    rm -rf /var/run/xrdp-sesman.pid
+    rm -rf /var/run/xrdp.pid
+    rm -rf /var/run/xrdp/*
+
+    # Use exec ... to forward SIGNAL to child processes
+    xrdp-sesman && exec xrdp -n
+}
+
+stop_xrdp_services() {
+    xrdp --kill
+    xrdp-sesman --kill
+    exit 0
+}
+
+echo -e "starting xrdp services...\n"
+
+trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+start_xrdp_services
